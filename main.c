33289a2123d9a6ef07f67923200d7946f288b0bd
Kuaaro <120309479+Kuaaro@users.noreply.github.com>
2022-12-11 18:27:56 +0100

    Update main.c v1.0
    Ostateczna poprawa błędów

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "determinants.h"
+#include "mat_io.h"
+
+int main(int argc, char **argv){
+    int i;
+    matrix *matr;
+    double_arr *arr;
+    FILE *f;
+
+    for(i=1; i<argc; i++){
+        f = fopen(argv[i], "r");
+        if(f!=NULL){
+            matr = make_matrix(f);
+            fclose(f);
+            if(matr!=NULL){
+                show_matr(matr);
+                arr = get_determ(matr);
+                if(arr!=NULL)
+                    solve_determ(arr);
+                else
+                    printf("Matryca nie jest kwadratowa\n");
+            }
+            free_matr(matr);
+        } else
+            printf("%s: %s nie istnieje\n\n", argv[0], argv[i]);
+    }
+    return 0;
+}
