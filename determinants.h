f2005ecf5c9018e0c4689efa0c93946cf2d63944
Throwaway12321 <120312911+Throwaway12321@users.noreply.github.com>
2022-12-11 15:59:55 +0100

    Pliki naglowkowe

---
+++ determinants.h
@@ -0,0 +1,15 @@
+#ifndef _DETERMINANTS_H_
+#define _DETERMINANTS_H_
+
+typedef struct{
+    double *list;
+    int size;
+}double_arr;
+
+void free_d_arr(double_arr *in); /*Zwalnia double_arr*/
+
+double_arr *get_determ(matrix *in); /*tworzy wyznaczniki macierzy*/
+
+int solve_determ(double_arr *in) /*Rozwiazuje rownanie z pomoca wyznacznikow oraz zwalnia wyznaczniki*/
+
+#endif
